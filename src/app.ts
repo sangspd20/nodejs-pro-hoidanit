97c2e4062972bd19c73280ad359db2bbada5ca69
sang nguyen <sangspd20@gmail.com>
2025-08-15 22:33:08 +0700

    30_env

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import 'dotenv/config';
 // const express = require('express');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello Sang vssss');
