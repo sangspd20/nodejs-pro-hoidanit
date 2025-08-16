e69cd2f56e564c0700eefd739e484c3f3642c44a
sang nguyen <sangspd20@gmail.com>
2025-08-16 18:37:31 +0700

    60_prisma_create

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,24 @@
-import { assert } from 'console';
+import { prisma } from 'config/client';
 import getConnection from '../config/db';
 
-const handleCreateUser = (name: string, email: string, address: string) => {};
+const handleCreateUser = async (
+  name: string,
+  email: string,
+  address: string
+) => {
+  await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      address: address,
+    },
+  });
+};
 const getAllUsers = async () => {
   const connection = await getConnection();
 
   try {
-    const [results, fields] = await connection.query('SELECT * FROM `users`');
+    const [results, fields] = await connection.query('SELECT * FROM `user`');
 
     return results;
   } catch (err) {
